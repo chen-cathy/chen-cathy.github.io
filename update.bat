a5fd09bb0d747aa574abca4934823363f5f89a8c
chen-cathy <eurobizlife@gmail.com>
2026-01-12 20:51:08 +0100

    Update page title and content

---
+++ update.bat
@@ -0,0 +1,8 @@
+@echo off
+git add .
+git commit -m "Update page title and content"
+git push
+echo ===============================
+echo   SUCCESS: Webpage Updated!
+echo ===============================
+pause
